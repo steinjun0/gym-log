c372d6ccfc3d0126be12db9297140d2489e8bc99
steinjun0 <1142308@naver.com>
2025-10-25 19:52:08 +0900

    feat: 상세 페이지 날짜 추가

--- app/gym/[gym-id]/page.tsx
+++ app/gym/[gym-id]/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { GYM_LOG_DUMMY_DATA } from "@/constants/gym-dummy";
+import { GymLog } from "@/types/gym";
+import { useState } from "react";
+
 export default function Gym() {
-  return <>hi gym</>;
+  /** @note 추후 더미데이터 API로 교체 예정 */
+  const [gymLog] = useState<GymLog>(GYM_LOG_DUMMY_DATA[0]);
+  return <DateTitle date={gymLog.date} />;
+}
+
+export function DateTitle({ date }: { date: Date }) {
+  return (
+    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance mb-12">
+      {`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`}
+    </h1>
+  );
 }
