c338a45850c2d732de468117b4c466812540b71e
steinjun0 <1142308@naver.com>
2025-10-25 19:11:48 +0900

    feat: 안쓴느 페이지 null 반환

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-16 px-16 bg-white dark:bg-black sm:items-start">
-        안씁니다
-      </main>
-    </div>
-  );
+  return null;
 }
